d720aab7c608588384c5e8c32aae6eabb7ec9a86
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2025-07-05 17:24:43 -0400

    Time: 5 ms (39.33%), Space: 19.7 MB (43.31%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -3,8 +3,10 @@ class Solution:
         nums.sort()
         length = len(nums)
         i = 0
-        while i < length:
-            if nums.count(nums[i]) == 1:
-                return nums[i]
-            else:
+        while i < length - 1:
+            if nums[i] == nums[i+1]:
                 i += 2
+            else:
+                return nums[i]
+        return nums[i]
+        
