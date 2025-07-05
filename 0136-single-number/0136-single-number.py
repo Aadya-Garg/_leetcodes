b0d24fc56a17ebf01a0f0f684bb6a130f0efb5af
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2025-07-05 17:17:11 -0400

    Time: 4069 ms (5.04%), Space: 19.6 MB (77.29%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,11 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        arr = []
-        for i in nums:
-            if i in arr:
-                continue
-            elif nums.count(i) == 1:
-                return i
-            else:
-                arr.append(i)
         
+        for i in nums:
+            if nums.count(i) == 1:
+                return i
