8b6d2857024870cbf26f7ab501c639dfb0df6b2e
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2025-07-24 23:33:15 -0400

    Time: 4 ms (15.05%), Space: 18 MB (26.75%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -0,0 +1,31 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        t = ""
+        if str1 not in str2 and str2 not in str1:
+            return ""
+        
+        len1 = len(str1)
+        len2 = len(str2)
+
+        if (len1 >= len2):
+            smaller = str2
+            min_ = len2
+        else:
+            smaller = str1
+            min_ = len1
+
+        #now loop on length of t (reference is smaller string)
+        for i in range(min_ - 1, -1, -1):
+            ref = i + 1
+            t = smaller[:ref]
+            if (len1 % ref != 0 or len2 % ref != 0):
+                continue
+            freq1 = len1 // ref
+            freq2 = len2 // ref
+            
+            if (t * freq1 == str1 and t * freq2 == str2):
+                return t
+        return ""
+
+
+        
