81644eb4027923db7c9b3168be91afdfb8a29933
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2025-08-13 17:44:24 -0400

    Time: 157 ms (27.81%), Space: 51.3 MB (77.54%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,7 +1,8 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         #number of overlapping intervals.
-        points.sort()
+        points.sort(key = lambda x: x[1])
+        print(points)
         try: 
             points[1]
         except:
@@ -10,12 +11,18 @@ class Solution:
         res = 1
         con = points[1:]
         for i,j in con:
+            if point < i:
+                res += 1
+                point = j
+            """
+            This is for when sorted normally
             if point >= i:
                 point = min(point, j)
             
             else:
                 res += 1
                 point = j
+            """
         return res
 
             
