b6ca3c6fe598cb2e9baea93fda3570bed9e2fc55
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2025-08-13 14:24:45 -0400

    Time: 922 ms (67.13%), Space: 35.1 MB (26.4%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
@@ -1,27 +1,13 @@
 class Solution:
     def minSum(self, nums1: List[int], nums2: List[int]) -> int:
         #minimizing problem
-        curr1 = 0
-        curr2 = 0
-        zer1 = 0
-        zer2 = 0
-        for i in nums1:
-            curr1 += i
-            if i == 0:
-                zer1 += 1
-        for j in nums2:
-            curr2 += j
-            if j == 0:
-                zer2 += 1
-        #curr1 = sum(nums1)
-        #curr2 = sum(nums2)
-        #zer1 = nums1.count(0)
-        #zer2 = nums2.count(0)
+        curr1 = sum(nums1)
+        curr2 = sum(nums2)
+        zer1 = nums1.count(0)
+        zer2 = nums2.count(0)
         min1 = curr1 + zer1
         min2 = curr2 + zer2
-        print(curr1, curr2)
-        print(min1, min2)
-        print(zer1, zer2)
+       
         if min1 == min2:
             return min1
 
