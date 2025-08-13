50c24ba89f35115204af247d671125d29b69c99c
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2025-08-13 17:36:40 -0400

    Time: 167 ms (23.21%), Space: 51.4 MB (77.54%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -6,19 +6,16 @@ class Solution:
             points[1]
         except:
             return 1
-        prev_j = sorted_intervals[0][1]
+        point = sorted_intervals[0][1]
         res = 1
-        point = prev_j
         con = sorted_intervals[1:]
         for i,j in con:
-
-            if i <= prev_j and point >= i:
+            if point >= i:
                 point = min(point, j)
             
             else:
                 res += 1
                 point = j
-            prev_j = j
         return res
 
             
