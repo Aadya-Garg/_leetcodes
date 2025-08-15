b6972e76e66ad5c08b0066307d1c63c9db7b0a15
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2025-08-15 12:51:42 -0400

    Time: 14 ms (73.84%), Space: 37.5 MB (23.11%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,7 +1,7 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        first = float('inf')
-        second = float('inf')
+        first = 2**31 - 1
+        second = 2**31 - 1
         for n in nums:
             if n <= first:
                 first = n
