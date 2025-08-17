7a56b37bc82b73ebe197b26a63af8e99b606a002
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2025-08-17 00:44:29 -0400

    Time: 38 ms (62.28%), Space: 23.2 MB (19.87%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.py
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.py
@@ -1,20 +1,28 @@
 class RecentCounter:
 
     def __init__(self):
-        self.requests = deque()
-
+        #self.requests = deque()
+        self.requests = []
+        self.start = 0
+        self.end = 0
 
     def ping(self, t: int) -> int:
         self.requests.append(t)
-        
+        self.end += 1
         #range is [t - 3000, t]
         lower = t-3000
+        while self.requests[self.start] < lower:
+            self.start += 1
+        return self.end - self.start
+        """
         while self.requests:
             if self.requests[0] < lower:
                 self.requests.popleft()
             else:
                 break
+        
         return len(self.requests)
+        """
 
 
         
