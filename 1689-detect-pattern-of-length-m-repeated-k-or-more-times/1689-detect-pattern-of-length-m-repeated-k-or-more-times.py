a75222a4a7643a12f67811e76f6432868de5ee81
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2025-08-27 14:04:45 +0400

    Time: 1 ms (28.6%), Space: 17.8 MB (62.8%) - LeetHub

--- 1689-detect-pattern-of-length-m-repeated-k-or-more-times/1689-detect-pattern-of-length-m-repeated-k-or-more-times.py
+++ 1689-detect-pattern-of-length-m-repeated-k-or-more-times/1689-detect-pattern-of-length-m-repeated-k-or-more-times.py
@@ -1,13 +1,9 @@
 class Solution:
     def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
         left = 0
-        #right = left + m - 1
         len_ = len(arr)
-        #count = 1
         done = True
-        print(len_ - k*m)
         while left <= len_ - k*m:
-            #print(count)
             for i in range(k - 1):
                 for j in range(m):
                     if arr[left + i*m + j] != arr[left + j + i*m + m]:
@@ -17,15 +13,8 @@ class Solution:
                     break
             if done:
                 return True
-            #else:
-                #count = 1
-                #done = True
             done = True
-
-            #if count == k:
-            #    return True
             left += 1
-            #right += 1
         return False
 
             
