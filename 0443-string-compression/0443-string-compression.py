ea97d06127329a5b40f6bfa3cafde3ccbed38d9b
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-05 17:19:06 -0400

    Time: 15 ms (3.22%), Space: 19.5 MB (35.65%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -12,19 +12,17 @@ class Solution:
             if chars[i] == char:
                 count += 1
                 chars[i] = " "
-                continue
 
-            if count == 1:
+            elif count == 1:
                 char = chars[i]
-                continue
-                
-            while count > 0:
-                chars[i - temp_ind] = str(count%10)
-                count = count // 10
-                temp_ind += 1
+            else:  
+                while count > 0:
+                    chars[i - temp_ind] = str(count%10)
+                    count = count // 10
+                    temp_ind += 1
 
-            count = 1
-            char = chars[i]
+                count = 1
+                char = chars[i]
         
         temp_ind = 1
         if chars[-1] == " ":
@@ -35,42 +33,4 @@ class Solution:
         
         while " " in chars:
             chars.remove(" ")
-
-
-
-
-
-        # length = len(chars)
-        # if (length == 1):
-        #     return len(chars)
-        # count = 1
-        # prev = chars[0]
-        # i = 1
-        # while i < length - 1:
-        #     if (prev == chars[i]):
-        #         count += 1
-        #         i += 1
-        #         continue
-
-        #     list_ = [prev]+ list(str(count))
-        #     prev = chars[i]
-
-        #     if (i - count >= 0):
-        #         chars = chars[:i - count] + list_ + chars[i:]
-        #     else:
-        #         chars = list_+chars[i:]
-        #     i += 1
-        #     count = 1
-
-        # if chars[-1] == prev:
-        #     chars = chars[:i - count] + [prev] + list(str(count + 1))
-        # else:
-        #     chars = chars[:i - count - 1] + [prev] + list(str(count + 1)) + chars[-1]
-        
-        # return len(chars)
-            
-
-            
-
-
-
+    
