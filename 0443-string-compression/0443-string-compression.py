30daa7f1b189acda27a6c9a374ef6c863f9ee058
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-05 17:30:45 -0400

    Time: 19 ms (3.22%), Space: 19.4 MB (35.65%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -3,29 +3,25 @@ class Solution:
         count = 1
         char = chars[0]
         
-        for i in range(1, len(chars)):
+        for i in range(1, len(chars) + 1):
             temp_ind = 1
-            if chars[i] == char:
+            if i < len(chars) and chars[i] == char:
                 count += 1
                 chars[i] = " "
                 continue
-            char = chars[i]
-
-            if count > 1:  
-                while count > 0:
-                    chars[i - temp_ind] = str(count%10)
-                    count = count // 10
-                    temp_ind += 1
-
+            char = chars[i] if i < len(chars) else ""
+            if count > 1:
+                count_ = list(str(count))
+                len_ = len(count_)  
+                chars[i - len_: i] = count_
                 count = 1
-        
-        temp_ind = 1
-        if chars[-1] == " ":
-            while count > 0:
-                chars[-temp_ind] = str(count%10)
-                count = count // 10
-                temp_ind += 1
         
         while " " in chars:
             chars.remove(" ")
+        
+        # if count > 1:
+        #         count_ = list(str(count))
+        #         len_ = len(count_)  
+        #         chars[i - len_: i] = count_
+        #         count = 1
     
