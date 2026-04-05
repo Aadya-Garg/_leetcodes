8cd92279196953990993a73063d007d237f0abd1
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-05 13:14:11 -0400

    Time: 7 ms (78.87%), Space: 19.6 MB (62.62%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,24 +1,42 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        if n == 0:
-            return True
-        # --- not all flowers filled yet -----
-        remaining = len(flowerbed)
-        if remaining == 0:
-            return False
+        # if n == 0:
+        #     return True
+        # # --- not all flowers filled yet -----
+        # remaining = len(flowerbed)
+        # if remaining == 0:
+        #     return False
 
-        if remaining == 1:
-            return (flowerbed[0] == 0 and n == 1)
+        # if remaining == 1:
+        #     return (flowerbed[0] == 0 and n == 1)
         
-        if flowerbed[1] == 1:
-            return self.canPlaceFlowers(flowerbed[3:], n)
+        # if flowerbed[1] == 1:
+        #     return self.canPlaceFlowers(flowerbed[3:], n)
 
-        if flowerbed[0] == 0 and flowerbed[1] == 0:
-            flowerbed[0] = 1
-            n -= 1
+        # if flowerbed[0] == 0 and flowerbed[1] == 0:
+        #     flowerbed[0] = 1
+        #     n -= 1
         
-        return self.canPlaceFlowers(flowerbed[2:], n)
+        # return self.canPlaceFlowers(flowerbed[2:], n)
         
-        
+        i = 0
+        while i < len(flowerbed):
+            if n == 0:
+                return True
+                
+            if i == len(flowerbed) - 1:
+                return (flowerbed[i] == 0 and n == 1)
 
-            
+            if flowerbed[i] == 0 and flowerbed[i+1] == 0:
+                flowerbed[i] = 1
+                n -= 1
+                i += 2
+                continue
+                
+            if flowerbed[i+1] == 1:
+                i += 3
+                continue
+
+            if flowerbed[i] == 1:
+                i += 2
+        return n == 0
