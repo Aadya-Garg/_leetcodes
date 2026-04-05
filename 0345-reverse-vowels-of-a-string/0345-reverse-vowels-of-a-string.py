24881edbe7ecc99b710c6479f25f0526102af7a5
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-05 13:45:26 -0400

    Time: 19 ms (22.6%), Space: 20.5 MB (83.61%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,15 +1,36 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
         vowels = ['a', 'e', 'i', 'o', 'u']
-        curr = []
-        for i in range(len(s)):
-            if s[i].lower() in vowels:
-                curr.append(s[i])
-        len_ = len(s)        
-        for i in range(len_):
-            if len(curr) == 0:
-                return s
+        l = 0
+        r = len(s) - 1
+        arr = list(s)
+        while l <= r:
+            left_v = arr[l].lower() in vowels
+            right_v = arr[r].lower() in vowels
+            if left_v and right_v:
+                temp = arr[l]
+                arr[l] = arr[r]
+                arr[r] = temp
+                l += 1
+                r -= 1
+                continue
 
-            if s[i].lower() in vowels:
-                s = s[:i] + curr.pop() + s[i+1:]
-        return s 
+            if not left_v:
+                l += 1
+
+            if not right_v:
+                r -= 1
+        return "".join(arr)
+            
+        # curr = []
+        # for i in range(len(s)):
+        #     if s[i].lower() in vowels:
+        #         curr.append(s[i])
+        # len_ = len(s)        
+        # for i in range(len_):
+        #     if len(curr) == 0:
+        #         return s
+
+        #     if s[i].lower() in vowels:
+        #         s = s[:i] + curr.pop() + s[i+1:]
+        # return s 
