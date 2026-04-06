56a0aeec95ec3ecce984b9e7488a28ae2c8986be
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-05 21:52:03 -0400

    Time: 66 ms (12.91%), Space: 29.6 MB (49.56%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,18 +1,35 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-
-        length = len(height)
-        max_area = 0
-        left = 0
-        right = length - 1
-        while left < right:
-            max_area = max(max_area, (right - left) * min(height[left], height[right]))
-
-            if height[left] < height[right]:
-                left += 1
+        l = 0
+        r = len(height) - 1
+        res = 0
+        while l < r:
+            temp_h = min(height[l], height[r])
+            temp_w = r - l
+            res = max(res, temp_h * temp_w)
+            #--- how do I move right and left here?
+            if height[l] > height[r]:
+                r -= 1
             else:
-                right -= 1
-        return max_area
+                l += 1
+        return res
+
+
+
+
+
+        # length = len(height)
+        # max_area = 0
+        # left = 0
+        # right = length - 1
+        # while left < right:
+        #     max_area = max(max_area, (right - left) * min(height[left], height[right]))
+
+        #     if height[left] < height[right]:
+        #         left += 1
+        #     else:
+        #         right -= 1
+        # return max_area
         """
         for i in range(length-1):
             if (height[i] == 0):
