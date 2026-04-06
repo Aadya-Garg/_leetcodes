5b12abbdcc81d7c3f3d8def522eccc893f822309
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-05 22:46:50 -0400

    Time: 218 ms (5.09%), Space: 29.2 MB (42.97%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        l = 0 #points to k elements
+        r = k #next element to be added to the k elems
+        temp_sum = sum(nums[: k])
+        res = temp_sum
+        while r < len(nums):
+            print(temp_sum)
+            temp_sum = temp_sum - nums[l] + nums[r]
+            l += 1
+            r += 1
+            res = max(res, temp_sum)
+        return res/k
