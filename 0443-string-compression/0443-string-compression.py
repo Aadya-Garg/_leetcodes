45157b669579bb9e7dcba8a843d5fccd092d496f
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-05 21:27:13 -0400

    Time: 3 ms (44.28%), Space: 19.5 MB (35.65%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,19 +1,36 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        count = 1
-        char = chars[0]
-        
-        for i in range(1, len(chars) + 1):
-            if i < len(chars) and chars[i] == char:
+        read = 0
+        write = 0
+        char = chars[read]
+        count = 0
+
+        for read in range(0, len(chars) + 1):
+            if read < len(chars) and chars[read] == char:
                 count += 1
-                chars[i] = " "
                 continue
-            char = chars[i] if i < len(chars) else ""
+            
+            chars[write] = char
+            write += 1
+            
             if count > 1:
-                count_ = list(str(count))
-                len_ = len(count_)  
-                chars[i - len_: i] = count_
+                for digit in str(count):
+                    chars[write] = digit
+                    write += 1
                 count = 1
+            char = chars[read] if read < len(chars) else ""
+        chars[:] = chars[: write]
+        # for i in range(1, len(chars) + 1):
+        #     if i < len(chars) and chars[i] == char:
+        #         count += 1
+        #         chars[i] = " "
+        #         continue
+        #     char = chars[i] if i < len(chars) else ""
+        #     if count > 1:
+        #         count_ = list(str(count))
+        #         len_ = len(count_)  
+        #         chars[i - len_: i] = count_
+        #         count = 1
         
-        while " " in chars:
-            chars.remove(" ")
+        # while " " in chars:
+        #     chars.remove(" ")
