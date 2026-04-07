f6a41278f5338f7a2162da0985f7f2d111eecc42
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-06 22:59:05 -0400

    Time: 101 ms (48.52%), Space: 28.3 MB (74.97%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,16 +1,17 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        after = [0]
+        
         min_days = 0
         n = len(temperatures)
+        after = [0] * n
         for i in range(n - 2, -1, -1):
             if temperatures[i+1] > temperatures[i]:
-                after.append(1)
-            elif after[n - i - 2] == 0:
-                after.append(0)
+                after[i] = 1
+            elif after[i + 1] == 0:
+                after[i] = 0
             else:
                 # see how many days the i + 1 has to wait, i has to wait that many days + 1 for sure
-                min_days = after[n - i - 2] + 1
+                min_days = after[i + 1] + 1
                 # now, 69 had to wait 1 day and the element at that day is 72, so we are done.
                 # oh so we have to keep adding like this until the day has higher value than curr
 
@@ -18,15 +19,13 @@ class Solution:
                 val = temperatures[i + min_days]
                 pointer = i + min_days #-- 2 + 2 + 1 = 5
                 while val <= temperatures[i]:
-                    # print(val, temperatures[i], pointer)
-                    if pointer >= n - 1 or after[n - pointer - 1]  == 0:
+                    if pointer >= n - 1 or after[pointer]  == 0:
                         min_days = 0
                         break
-                    min_days += after[n - pointer - 1] # 8 - 5 - 1 = 2 val = 1
-                    pointer += after[n - pointer - 1] # 6
+                    min_days += after[pointer] # 8 - 5 - 1 = 2 val = 1
+                    pointer += after[pointer] # 6
                     val = temperatures[pointer]
-                after.append(min_days)
-        after.reverse()
+                after[i] = min_days
     
         return after
 
