3cbb130f8f193de5ffae8eea1903ed9eacbb99e8
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-10 19:40:15 -0400

    Time: 100 ms (49.38%), Space: 28.5 MB (60.51%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,31 +1,51 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        
-        min_days = 0
         n = len(temperatures)
-        after = [0] * n
-        for i in range(n - 2, -1, -1):
-            if temperatures[i+1] > temperatures[i]:
-                after[i] = 1
-            elif after[i + 1] == 0:
-                after[i] = 0
+        res = [0]*n
+        remaining = [] #--- indices
+        for i in range(0, n):
+            while remaining:
+                if temperatures[i] > temperatures[remaining[-1]]:
+                    ind = remaining.pop()
+                    res[ind] = i - ind
+                else:
+                    break
+            if i == n - 1:
+                break
+            if temperatures[i + 1] > temperatures[i]:
+                res[i] = 1
             else:
-                # see how many days the i + 1 has to wait, i has to wait that many days + 1 for sure
-                min_days = after[i + 1] + 1
-                # now, 69 had to wait 1 day and the element at that day is 72, so we are done.
-                # oh so we have to keep adding like this until the day has higher value than curr
+                remaining.append(i)
+        for j in remaining:
+            res[j] = 0
+        return res
+            
 
-                #---- check the index calculations thoroughly ----
-                pointer = i + min_days #-- 2 + 2 + 1 = 5
-                while temperatures[pointer] <= temperatures[i]:
-                    if pointer >= n - 1 or after[pointer]  == 0:
-                        min_days = 0
-                        break
-                    min_days += after[pointer] # 8 - 5 - 1 = 2 val = 1
-                    pointer += after[pointer] # 6
-                after[i] = min_days
+        # min_days = 0
+        # n = len(temperatures)
+        # after = [0] * n
+        # for i in range(n - 2, -1, -1):
+        #     if temperatures[i+1] > temperatures[i]:
+        #         after[i] = 1
+        #     elif after[i + 1] == 0:
+        #         after[i] = 0
+        #     else:
+        #         # see how many days the i + 1 has to wait, i has to wait that many days + 1 for sure
+        #         min_days = after[i + 1] + 1
+        #         # now, 69 had to wait 1 day and the element at that day is 72, so we are done.
+        #         # oh so we have to keep adding like this until the day has higher value than curr
+
+        #         #---- check the index calculations thoroughly ----
+        #         pointer = i + min_days #-- 2 + 2 + 1 = 5
+        #         while temperatures[pointer] <= temperatures[i]:
+        #             if pointer >= n - 1 or after[pointer]  == 0:
+        #                 min_days = 0
+        #                 break
+        #             min_days += after[pointer] # 8 - 5 - 1 = 2 val = 1
+        #             pointer += after[pointer] # 6
+        #         after[i] = min_days
     
-        return after
+        # return after
 
 
             
