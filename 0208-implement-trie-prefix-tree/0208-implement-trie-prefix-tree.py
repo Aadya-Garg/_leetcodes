b29e2925b009df4e09ff90485de1511d04a958e4
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-12 17:46:33 -0400

    Time: 40 ms (50.05%), Space: 34.9 MB (17.49%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -2,31 +2,23 @@ class Trie:
 
     def __init__(self):
         self.prefixes = {}
-        self.isEnd = False
 
     def insert(self, word: str, index: int = 0) -> None:
         curr = self
+        word += "$"
         for char in word:
             if char not in curr.prefixes:
                 curr.prefixes[char] = Trie()
             curr = curr.prefixes[char]
-        curr.isEnd = True
-        # try:
-        #     if word[index] not in self.prefixes:
-        #         self.prefixes[word[index]] = Trie() # we keep adding keys
-
-        #     self.prefixes[word[index]].insert(word, index + 1)
-        # except IndexError:
-        #     self.isEnd = True
-        #     return
 
     def search(self, word: str, index: int = 0) -> bool:
         curr = self
+        word += "$"
         for char in word:
             if char not in curr.prefixes:
                 return False
             curr = curr.prefixes[char]
-        return curr.isEnd
+        return True
 
     def startsWith(self, prefix: str, index: int = 0) -> bool:
         curr = self
