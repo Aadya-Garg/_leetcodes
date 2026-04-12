b455dc7b9ba1e6478893469939451f0137707e42
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-12 17:21:46 -0400

    Time: 51 ms (60.48%), Space: 24.8 MB (46.21%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -4,49 +4,48 @@ class Solution:
 
     def insert(self, word: str, index: int = 0) -> None:
         curr_prefixes = self.prefixes
-        for char in word:
-            if char not in curr_prefixes:
+        for char in word: # so total = O(length of word)
+            if char not in curr_prefixes: # constant
                 curr_prefixes[char] = {"words": []}
 
-            if len(curr_prefixes[char]["words"]) < 3:
+            if len(curr_prefixes[char]["words"]) < 3: # constant
                 curr_prefixes[char]["words"].append(index)
 
-            curr_prefixes = curr_prefixes[char]
+            curr_prefixes = curr_prefixes[char] #constant
 
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         #---- build trie ---
-        products.sort()
-        l = 0
-        r = len(products) - 1
-        res = []
-        char = ""
-        for i in range(len(searchWord)):
-            char += searchWord[i]
-            while l <= r:
-                if products[l].startswith(char):
-                    break
-                l += 1
-            while l <= r:
-                if products[r].startswith(char):
-                    break
-                r -= 1
-            
-            res.append(products[l:min(l+3, r + 1)]) if l <= r else res.append([])
-        return res
-        # --- now l points to the word that is >= m 
-        # r points to a word that is <= m    
-        # for i in range(len(products)):
-        #     self.insert(products[i], i)
-            
-        # res =[]
-        # curr = self.prefixes
+        products.sort() #O(nlogn)
+        # l = 0
+        # r = len(products) - 1
+        # res = []
+        # char = ""
         # for i in range(len(searchWord)):
-        #     char = searchWord[i]
-        #     if char not in curr:
-        #         curr = {}
-        #         res.append([])
-        #     else:
-        #         res.append([products[ind] for ind in curr[char]["words"]])
-        #         curr = curr[char]
+        #     char += searchWord[i]
+        #     while l <= r:
+        #         if products[l].startswith(char):
+        #             break
+        #         l += 1
+        #     while l <= r:
+        #         if products[r].startswith(char):
+        #             break
+        #         r -= 1
+            
+        #     res.append(products[l:min(l+3, r + 1)]) if l <= r else res.append([])
+        # return res
+
+        for i in range(len(products)): #O(total # of characters in products)
+            self.insert(products[i], i)
+            
+        res =[]
+        curr = self.prefixes
+        for i in range(len(searchWord)):
+            char = searchWord[i]
+            if char not in curr:
+                curr = {}
+                res.append([])
+            else:
+                res.append([products[ind] for ind in curr[char]["words"]])
+                curr = curr[char]
 
         return res
