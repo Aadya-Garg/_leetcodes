10ef665ab69d77cc67949ab5e69a577d4b633cf6
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-12 17:04:59 -0400

    Time: 3 ms (95.08%), Space: 22.3 MB (68.94%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -16,18 +16,37 @@ class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         #---- build trie ---
         products.sort()
-        for i in range(len(products)):
-            self.insert(products[i], i)
-            
-        res =[]
-        curr = self.prefixes
+        l = 0
+        r = len(products) - 1
+        res = []
+        char = ""
         for i in range(len(searchWord)):
-            char = searchWord[i]
-            if char not in curr:
-                curr = {}
-                res.append([])
-            else:
-                res.append([products[ind] for ind in curr[char]["words"]])
-                curr = curr[char]
+            char += searchWord[i]
+            while l <= r:
+                if products[l].startswith(char):
+                    break
+                l += 1
+            while l <= r:
+                if products[r].startswith(char):
+                    break
+                r -= 1
+            
+            res.append(products[l:min(l+3, r + 1)]) if l <= r else res.append([])
+        return res
+        # --- now l points to the word that is >= m 
+        # r points to a word that is <= m    
+        # for i in range(len(products)):
+        #     self.insert(products[i], i)
+            
+        # res =[]
+        # curr = self.prefixes
+        # for i in range(len(searchWord)):
+        #     char = searchWord[i]
+        #     if char not in curr:
+        #         curr = {}
+        #         res.append([])
+        #     else:
+        #         res.append([products[ind] for ind in curr[char]["words"]])
+        #         curr = curr[char]
 
         return res
