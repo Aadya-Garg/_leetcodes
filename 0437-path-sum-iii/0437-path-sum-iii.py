6c3a4b46b76ebcdd988221976fb810cf5f9654c5
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-12 22:53:09 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -11,26 +11,20 @@ class Solution:
             return 0
         stack = [(root, [])]
         res = 0
-        # if root.val == targetSum:
-        #     res = 1
-        # if root.right:
-        #      stack.append((root.right, [targetSum - root.val]))
-        # if root.left:
-        #     stack.append((root.left, [targetSum - root.val]))
 
         while stack:
-            node, s = stack.pop()
-            sum_new = [0]*len(s)
+            node, prevSums = stack.pop()
+            sum_new = [0]*len(prevSums)
             # append the look up vals to sums
             curr_val = node.val
                 
-            for i in range(len(s)):
-                sum_new[i] = s[i] - curr_val
+            for i in range(len(prevSums)):
+                sum_new[i] = sprevSumsi] - curr_val
                 if sum_new[i] == 0:
                     res += 1
-            
-            sum_new.append(targetSum - curr_val) #yes it can be 0 that I am looking for
-            if sum_new[-1] == 0:
+            diff = targetSum - curr_val
+            sum_new.append(diff) #yes it can be 0 that I am looking for
+            if diff == 0:
                 res += 1
 
             if node.right:
