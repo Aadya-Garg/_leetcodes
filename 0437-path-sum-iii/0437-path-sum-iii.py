08149d2d6ef697879d4c6c949905cdcaebe8f4d4
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-04-12 22:53:52 -0400

    Time: 135 ms (29.44%), Space: 19.7 MB (98.57%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -15,14 +15,12 @@ class Solution:
         while stack:
             node, prevSums = stack.pop()
             sum_new = [0]*len(prevSums)
-            # append the look up vals to sums
-            curr_val = node.val
                 
             for i in range(len(prevSums)):
-                sum_new[i] = sprevSumsi] - curr_val
+                sum_new[i] = prevSums[i] - node.val
                 if sum_new[i] == 0:
                     res += 1
-            diff = targetSum - curr_val
+            diff = targetSum - node.val
             sum_new.append(diff) #yes it can be 0 that I am looking for
             if diff == 0:
                 res += 1
