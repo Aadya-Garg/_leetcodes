6c9f2c8ffce8c1e85b30fc3d7938f56efdc5fea6
Aadya Garg <128272242+Aadya-Garg@users.noreply.github.com>
2026-05-01 17:32:45 +0300

    Time: 0 ms (100%), Space: 19 MB (99.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,10 +1,9 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        d = {0: 1}
+        d = [1]
           
         def factorial(num):
-            for i in range(1, num + 1):
-                if i not in d:
-                    d[i] = i * d[i - 1]
+            for i in range(len(d), num + 1):
+                d.append(i * d[i - 1])
             return d[num]
         return int(factorial(m + n - 2)/(factorial(m-1)* factorial(n - 1)))
